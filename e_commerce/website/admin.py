fa71e7893faf5c78c3cf66156f562511afc38080
HARTHIK-123 <kuppamharthik@gmail.com>
2025-09-27 10:43:23 +0530

    website.Authuser fixed

--- e_commerce/website/admin.py
+++ e_commerce/website/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from website.models import Products,AuthUser
+from website.models import Products,Authuser
 # Register your models here.
 admin.site.register(Products)
-admin.site.register(AuthUser)
+admin.site.register(Authuser)
 # class AdminProduct(admin.ModelAdmin):
     # model = Products
     # list_display =('id','name','price','stock')
