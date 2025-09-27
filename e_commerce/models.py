c334de711e1921af14c792cd4f2b0fa6d348b941
HARTHIK-123 <kuppamharthik@gmail.com>
2025-09-27 09:54:34 +0530

    Deleted migrations files

--- e_commerce/models.py
+++ e_commerce/models.py
@@ -1,13 +1,13 @@
 import django.db.models as model
 from django.contrib.auth.models import AbstractUser
 
-
+class AuthUser(AbstractUser):   # <-- Check the exact spelling/capitalization
+    # any extra fields
+    pass
 
 class Products(model.Model):
     #ID field is auto-created by Django as primary key
     name = model.CharField(max_length=250)
     description =  model.TextField()
     price = model.DecimalField(max_digits=10,decimal_places=2)
-class AuthUser(AbstractUser):   # <-- Check the exact spelling/capitalization
-    # any extra fields
-    pass
+
